8d0433e53b232a289f6edfaa4196f83930d903d3
Arbalest-bhp <100734513+Arbalest-bhp@users.noreply.github.com>
2022-03-13 23:05:46 -0500

    Add files via upload

---
+++ iFraccion.java
@@ -0,0 +1,10 @@
+
+public interface iFraccion {
+            public Fraccion dividir(double num, double denom);
+            public Fraccion multiplicar(double num, double denom);
+            public Fraccion sumar (double num, double denom);
+            public Fraccion restar (double num, double denom);
+            public Fraccion potencia (double poten);
+            public Fraccion raizCuadrada ();
+            public Fraccion porcentajeDecimal ();   
+}
